8bd444a57b4de7b724a618137ea4b98665e721e5
onishi taisei <taisei.developer@gmail.com>
2022-08-31 00:00:58 +0900

    🚀 feat: update stripe log function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,23 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp(functions.config().firebase);
+export const firestore = admin.firestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const addPaymentLog = functions.https.onRequest(async (req, res) => {
+  const event = req.body;
+
+  const ref = firestore.collection("payment").doc();
+  const obj: any = event.data.object;
+  await ref.set({
+    uid: event.id,
+    type: event.type,
+    amount: obj.amount_total,
+    name: obj.customer_details.name,
+    email: obj.customer_details.email,
+    used: true,
+    createdAt: admin.firestore.FieldValue.serverTimestamp,
+    updatedAt: admin.firestore.FieldValue.serverTimestamp,
+  });
+
+  res.end();
+});
